9042b12fbd89a2c608109a429bcd1bdc6ae6ca9d
bona1507 <bnvt15@student.ub.ac.id>
2024-09-03 17:04:01 +0700

    init

---
+++ app/src/main/java/com/tifd/projectcomposed/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.tifd.projectcomposed
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.tifd.projectcomposed.ui.theme.ProjectComposeDTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ProjectComposeDTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MyScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyScreen() {
+    var text by remember { mutableStateOf("") }
+    var inputText by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = text)
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(
+            value = inputText,
+            onValueChange = { inputText = it },
+            label = { Text("Enter text") }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            text = inputText
+        }) {
+            Text("Submit")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ProjectComposeDTheme {
+        MyScreen()
+    }
+}
